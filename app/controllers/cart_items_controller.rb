20931c60d6ea58fe4a8ebee58cbbef89bdb22894
EC2 Default User <ec2-user@ip-172-31-21-137.ap-northeast-1.compute.internal>
2023-04-18 09:05:06 +0000

    [Add]カートアイテム機能

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,28 +1,30 @@
 class CartItemsController < ApplicationController
-  
+
   def index
+    #@cart_items = current_customer.cart_items
     @cart_items = CartItem.all
-    @sum
+
+    @sum = 0
   end
-  
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(quantity: params[:cart_item][:quantity])
     redirect_to cart_items_path
   end
-  
+
   def destroy
     @item = CartItem.find(params[:id])
     @item.destroy
     redirect_to cart_items_path
   end
-  
+
   def destroy_all
     @item = current_customer.cart_items
     @item.destroy_all
     redirect_to cart_items_path
   end
-  
+
   def create
     if @cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       cart_item = @cart_items.find_by(item_id: params[:cart_item][:item_id])
@@ -34,10 +36,11 @@ class CartItemsController < ApplicationController
       redirect_to cart_items_path
     end
   end
-  
+
   private
-  
+
   def cart_item_params
     params.require(:cart_item).parmit(:item_id, :quantity)
   end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,8 @@
 class CartItem < ApplicationRecord
+  
   belongs_to :customer
   belongs_to :item
+  
   def subtotal
     item.tax_item_price * quantity
   end
