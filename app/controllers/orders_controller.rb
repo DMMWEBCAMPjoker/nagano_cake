f55f07eb4af5eecbdb045cdd658e2f80fc7869fd
EC2 Default User <ec2-user@ip-172-31-21-137.ap-northeast-1.compute.internal>
2023-04-19 10:56:49 +0000

    [Fix]画像

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,36 +8,26 @@ class OrdersController < ApplicationController
 
   def complete
   end
-  
+
   def create
   end
 
   def index
-<<<<<<< HEAD
-    @orders = current_customer.orders
-    @sum = 0
-=======
     @order = Order.find(params[:id])
     @sum= 0
->>>>>>> origin/develop
   end
 
   def show
   end
-  
+
   private
-  
+
   def order_params
     params.require(:order).permit(:payment_method, :delivery_name, :delivery_address, :delivery_postcode, :postage, :invoice)
   end
-  
-<<<<<<< HEAD
+
   def delivery_address_params
     params.require(:order).permit(:delivery_name, :delivery_address, :delivery_postcode)
-=======
-  def deliveries_params
-    params.require(:oeder).permit(:name, :address, :postcode)
->>>>>>> origin/develop
   end
-  
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,14 +1,9 @@
 class CartItem < ApplicationRecord
-  
+
   belongs_to :customer
   belongs_to :item
-  
+
   def subtotal
-<<<<<<< HEAD
     item.add_tax_price * quantity.to_i
-=======
-    item.add_tax_price * quantity
->>>>>>> origin/develop
   end
-  
 end
