dc95021005a719b30b7ebce5a1b2c6e968f0f352
EC2 Default User <ec2-user@ip-172-31-21-137.ap-northeast-1.compute.internal>
2023-04-20 05:47:40 +0000

    機能追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,36 @@
 class OrdersController < ApplicationController
+  
   def new
-    @order = orders.new
-    @customer = Customer.find(surrent_customer.id)
-    @delivery_address = current_customer.destinations
+    @order = Order.new
   end
 
   def check
+    @cart_items = current_customer.cart_items.all
+    @order = Order.new(order_params)
+    @sum = 0
+    @order.postage = 800
+    if params[:order][:address_number] == "1"
+      @order.delivery_name = current_customer.last_name + current_customer.first_name
+      @order.delivery_address = current_customer.address
+      @order.delivery_postcode = current_customer.postcode
+
+    elsif params[:order][:address_number]  == "2"
+      if Dekivery.exists?(id: params[:order][:address_id])
+        @delivery = Delivery.find(params[:order][:address_id])
+        @order.address = @deliveries.address
+        @order.postcode = @deliveries.postcode
+        @order.name = @deliveries.name
+      else
+        render :new
+      end
+
+    elsif params[:order][:address_number] == "3"
+      delivery_new = current_customer.deliveries.new(delivery_params)
+      if delivery_new.save
+      else
+        render :new
+      end
+    end
   end
 
   def complete
@@ -28,8 +53,8 @@ class OrdersController < ApplicationController
     params.require(:order).permit(:payment_method, :delivery_name, :delivery_address, :delivery_postcode, :postage, :invoice)
   end
 
-  def delivery_address_params
-    params.require(:order).permit(:delivery_name, :delivery_address, :delivery_postcode)
+  def delivery_params
+    params.require(:order).permit(:name, :address, :postcode)
   end
 
 end
